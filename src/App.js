dffa60d06eab909c8b267955927e582fb2f606bd
Emanuel Coitiño Leal <fmanuleal@gmail.com>
2021-02-11 10:19:27 -0300

    architect-style-imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,16 @@
 import Dashboard from './components/Dashboard';
 import Sidebar from "./components/Sidebar";
-import Head from "./components/Head";
 import './App.css';
 
 function App() {
+
 	return (
-		<div className="App">
-			<Head/>
-			<div id="right-panel" class="right-panel">
+		<>
+			<body>
 				<Dashboard />
-			</div>
-			<Sidebar/>
-		</div>
+				<Sidebar/>
+			</body>
+		</>	
 	);
 }
 
---
+++ src/components/Footer.jsx
@@ -0,0 +1,18 @@
+const Footer = () => {
+    return (
+        <footer className="site-footer">
+            <div className="footer-inner bg-white">
+                <div className="row">
+                    <div className="col-sm-6">
+                        Copyright &copy; 2021 Trainning Fit
+                </div>
+                    <div className="col-sm-6 text-right">
+                        Designed by <a href="https://colorlib.com">Colorlib</a>
+                    </div>
+                </div>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
